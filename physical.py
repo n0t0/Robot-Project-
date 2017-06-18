4a4cf1c09baeedf6ea8845cf3ee4ee0baa998994
Ivo Ivanov <vn.n0t0@gmail.com>
2017-06-18 15:00:23 -0700

    Physical Characteristics Class -  added

---
+++ physical.py
@@ -0,0 +1,27 @@
+class Physical():
+
+    skills = ['running', 'swimming', 'jumping']
+
+
+    def __init__(self, name, owner, physicalChars):
+        self.owner = owner
+        self.name = name
+
+    def greeting(self):
+        print 'Hi {}. My owner is {}'.format(self.name, self.owner)
+        print 'It is nice to meet you'
+
+
+    def physicalChars(self, color, height, weight, hair, eyes):
+        print 'COLOR:\t %s\n HEIGHT:\t %s\n EYES:\t %s\n' % (self.color, self.height, self.eyes)
+
+
+owner = 'Bongo'
+name = 'Maya'
+physicalChars = 'black'
+
+maya = Physical(name, owner, physicalChars)
+
+print maya.physicalChars
+print maya.greeting()
+print maya.skills
