f06999fd0886bc7c811e657f9a15692f5d693889
Ivo Ivanov <vn.n0t0@gmail.com>
2017-06-26 19:33:52 -0700

    emotion() added

--- brain.py
+++ brain.py
@@ -1,48 +1,65 @@
-# Ra (Robot Adviser)
+# BRF
 
 
 owner = 'Bongo'
-name = ['Mila', 'Maya', 'Maria']
+friends = ['mila', 'maya', 'maria'] # list of friends
 behavior = ['good', 'bad']
-mood = ['happy', 'sad']
+mood = ['good', 'bad', 'happy', 'sad'] # list of common moods
+
 
 def hello():
-    user = raw_input('What is your name? ')
-    if user in name:
-        print 'Hi', user
-        print raw_input('How are you? ')
+    user = raw_input('What is your name? ').lower()
+    if user in friends:
+        print 'Hi', user.upper()
+        # mv ln24 def emotion(): function checking for user's state
+        # pass emotion() as an argument to behDecision()
     else:
-        print 'Hi {}. Nice to meet you. My owner is {}'.format(user, owner)
-        print 'Analysing...'
-        print 'Implanting...'
-        print 'Added to database'
+        print 'Hi {}. Nice to meet you. My owner is {}'.format(user, owner.upper())
+        print 'Analysing...'    # func needed (Amazon Rekognition)
+        print 'Implanting...'   # func needed (Amazon Polly)
+        print 'Added to database'   # lambda func to DynamoDB
 
 print hello()
 
-print '===='*40
-print '||||'*40
-print '===='*40
+
+def emotion():
+    m = raw_input('How are you? ').lower()
+    while m in mood:
+        if m == 'good':
+            print 'Happy face'
+            print ':)'
+            break
+        elif m == 'bad' or 'sad':
+            print "I'm sorry. Cheer up."
+            break
+        else:
+            print 'Tell more about it'
+            print 'What made you', m + '?'
+            break
+    else:
+        print 'Emotion not detected'
+        print 'Ask another question'
+
+print emotion()
 
 # from physical import Physical
+def behDecision(emotion):
 
-def behDecision(behavior, mood):
+    # Behavioral Decision compares emotional and physical arguments
+    # The result calls for -- implanting function
 
     '''
     Robot makes a decision based on the behavior
     '''
 
-    for b in behavior: # needs a while loop
-        if b == 'good':
-            print 'Your behavior is GOOD', name[0]
-            for m in mood:
-                if m == 'sad':
-                    print 'But you are SAD'
-            break
-
-        else:
-            print 'You behavior is bad'
-            print 'Do NOT miss behave'
-            print 'Adsive with', owner
+    if m in emotion == 'good':
+        print 'Your behavior is good'
+    #elif:
+    #    print 'You behavior is bad'
+    #    print 'Do NOT miss behave'
+    #    print 'Adsive with', owner
+    else:
+        print 'Take action' # calls another functions
 
 
-print behDecision(behavior, mood)
+print behDecision(emotion)
