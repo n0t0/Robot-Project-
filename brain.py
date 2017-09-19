af7affee05a41ece16d4e2a1c095be4d3f0cd5aa
Ivo Ivanov <vn.n0t0@gmail.com>
2017-09-18 18:21:28 -0700

    brain - updated

--- brain.py
+++ brain.py
@@ -1,19 +1,20 @@
 # BRF ~ Bsto Robo Frnd
 
 owner = 'Bongo'
+home = 'Gipsy Mahala'
 friends = ['mila', 'maya', 'maria'] # list of friends
 mood = ['good', 'bad', 'happy', 'sad', 'lazy'] # list of common moods
 #behavior = ['good', 'bad']
 
 
 def hello():
-    user = raw_input('What is your name? ').lower()
+    user = raw_input('What is your name?\n').lower()
     if user in friends:
         print 'Hi', user.upper()
-        # mv ln24 def emotion(): function checking for user's state
-        # pass emotion() as an argument to behDecision()
+        # mv ln24 def emotion_detect(): function checking for user's state
+        # pass emotion_detect() as an argument to behDecision()
     else:
-        print 'Hi {}. Nice to meet you. My owner is {}'.format(user, owner.upper())
+        print 'Hi {}. It is nice to meet you! My owner is {}'.format(user, owner.upper())
         print 'Analysing...'    # func needed (Amazon Rekognition)
         print 'Implanting...'   # func needed (Amazon Polly)
         print 'Added to database'   # lambda func to DynamoDB
@@ -21,19 +22,19 @@ def hello():
 print hello()
 
 
-def emotion():
+def emotion_detect():
 
     # ToDo:
     # pass the output to a list for comparison
     # Try emotion(mood)
 
-    m = raw_input('How are you? ').lower()
+    m = raw_input('How are you?\n').lower()
     while m in mood:
-        if m == 'good':
-            print 'Happy face'
-            print ':)'
-            break       # loop is not breaking when mood is 'good'
-        elif m == 'bad' or 'sad':
+        if m == 'good':    # list all good moods
+            print 'Happy face. :)'
+            print 'Do you want to play a game?'   # input/sync with games
+            break
+        elif m == 'bad':    # list all bad moods
             print "I'm sorry. Cheer up."
             break
         else:   # the rest of the list with moods goes here
@@ -44,25 +45,24 @@ def emotion():
         print 'Emotion not detected'
         print 'Ask another question'
 
-#print emotion()
+print emotion_detect()
 
 # from physical import Physical
 
-behavior = str(emotion())  # functions: behCheck(), emotion()
+# behavior = str(emotion_detect())  # functions: behCheck(), emotion_detect()
 
-
-def behCheck(emotion):
-
-    # Behavioral Check compares emotional and physical arguments
-    # The result calls for -- implanting function
-
-    '''     Robot makes a decision based on the behavior     '''
-    for b in behavior:
-        while behavior == 'good':
-            print 'Good'
-            break
-        else:
-            print 'Take action' # call another function
+# def behCheck(emotion):
+#
+#     # Behavioral Check compares emotional and physical arguments
+#     # The result calls for -- implanting function
+#
+#     '''     Robot makes a decision based on the behavior     '''
+#     for b in behavior:
+#         while behavior == 'good':
+#             print 'Good'
+#             break
+#         else:
+#             print 'Take action' # call another function
     #for b in behavior:
     #    print 'Hmm. We need to pass a few more arguments'
 
@@ -80,6 +80,6 @@ def behCheck(emotion):
     #else:
         #print 'Take action' # calls another functions
 
-    #print emotion()
+    #print emotion_detect()
 
-print behCheck(emotion)
+# print behCheck(emotion)
