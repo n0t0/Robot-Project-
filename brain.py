3842d98807237168338fa870c7eb4eeb17e844bf
Ivo Ivanov <vn.n0t0@gmail.com>
2018-08-29 21:03:41 -0700

    file - update

--- brain.py
+++ brain.py
@@ -8,18 +8,18 @@ mood = ['good', 'bad', 'happy', 'sad', 'lazy'] # list of common moods
 
 
 def hello():
-    user = raw_input('What is your name?\n').lower()
+    user = input('What is your name?\n').lower()
     if user in friends:
-        print 'Hi', user.upper()
+        print ('Hi'), user.upper()
         # mv ln24 def emotion_detect(): function checking for user's state
         # pass emotion_detect() as an argument to behDecision()
     else:
-        print 'Hi {}. It is nice to meet you! My owner is {}'.format(user, owner.upper())
-        print 'Analysing...'    # func needed (Amazon Rekognition)
-        print 'Implanting...'   # func needed (Amazon Polly)
-        print 'Added to database'   # lambda func to DynamoDB
+        print ('Hi {}. It is nice to meet you! My owner is {}'.format(user, owner.upper()))
+        print ('Analysing...')    # func needed (Amazon Rekognition)
+        print ('Implanting...')  # func needed (Amazon Polly)
+        print ('Added to database')   # lambda func to DynamoDB
 
-print hello()
+print (hello())
 
 
 def emotion_detect():
@@ -28,24 +28,24 @@ def emotion_detect():
     # pass the output to a list for comparison
     # Try emotion(mood)
 
-    m = raw_input('How are you?\n').lower()
+    m = input('How are you?\n').lower()
     while m in mood:
         if m == 'good':    # list all good moods
-            print 'Happy face. :)'
-            print 'Do you want to play a game?'   # input/sync with games
+            print ('Happy face. :)')
+            print ('Do you want to play a game?')   # input/sync with games
             break
         elif m == 'bad':    # list all bad moods
-            print "I'm sorry. Cheer up."
+            print ("I'm sorry. Cheer up.")
             break
         else:   # the rest of the list with moods goes here
-            print 'Tell more about it'
-            print 'What made you', m + '?'
+            print ('Tell more about it')
+            print ('What made you'), m + ('?')
             break
     else:
-        print 'Emotion not detected'
-        print 'Ask another question'
+        print ('Emotion not detected')
+        print ('Ask another question')
 
-print emotion_detect()
+print (emotion_detect())
 
 # from physical import Physical
 
